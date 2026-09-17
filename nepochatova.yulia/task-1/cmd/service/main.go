84ea44807cb407ab286d0fbc7e80a646d7875762
GenJune <julia.nepochatova@gmail.com>
2026-09-17 23:06:06 +0300

    added basic operations

--- nepochatova.yulia/task-1/cmd/service/main.go
+++ nepochatova.yulia/task-1/cmd/service/main.go
@@ -1,1 +1,39 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	var operation string
+
+	fmt.Print("Enter first number ")
+	fmt.Scan(&a)
+
+	fmt.Print("Enter operation ")
+	fmt.Scan(&operation)
+
+	fmt.Print("Enter second number ")
+	fmt.Scan(&b)
+
+	switch operation {
+
+	case "+":
+		fmt.Println(a + b)
+
+	case "-":
+		fmt.Println(a - b)
+
+	case "*":
+		fmt.Println(a * b)
+
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(a / b)
+		}
+
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
