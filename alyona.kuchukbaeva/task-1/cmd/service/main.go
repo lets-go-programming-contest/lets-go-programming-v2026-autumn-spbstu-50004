6594642e632a1303ff7488a01c94e2c9e8278e64
SoundwaveAI <ghost10.town10000@gmail.com>
2026-09-17 19:13:16 +0300

    [TASK-1] fix formatting

--- alyona.kuchukbaeva/task-1/cmd/service/main.go
+++ alyona.kuchukbaeva/task-1/cmd/service/main.go
@@ -8,33 +8,33 @@ func main() {
 
     if _, err := fmt.Scanln(&a); err != nil {
         fmt.Println("Invalid first operand")
-	return
+        return
     }
 
     if _, err := fmt.Scanln(&b); err != nil {
-	fmt.Println("Invalid second operand")
-	return
+        fmt.Println("Invalid second operand")
+        return
     }
 
     if _, err := fmt.Scanln(&op); err != nil {
-	fmt.Println("Invalid operation")
-	return
+        fmt.Println("Invalid operation")
+        return
     }
 
     switch op {
     case "+":
-	fmt.Println(a + b)
+        fmt.Println(a + b)
     case "-":
-	fmt.Println(a - b)
+        fmt.Println(a - b)
     case "*":
-	fmt.Println(a * b)
+        fmt.Println(a * b)
     case "/":
-	if b == 0 {
-	    fmt.Println("Division by zero")
+        if b == 0 {
+            fmt.Println("Division by zero")
             return
-	}
-	fmt.Println(a / b)
+        }
+        fmt.Println(a / b)
     default:
-	fmt.Println("Invalid operation")
+        fmt.Println("Invalid operation")
     }
 }
