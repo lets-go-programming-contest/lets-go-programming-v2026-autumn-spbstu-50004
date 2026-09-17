576926b5dce4f5a65b80053ae88a29205c59ae44
SoundwaveAI <ghost10.town10000@gmail.com>
2026-09-17 19:23:47 +0300

    [TASK-1] fix formatting 2.0

--- alyona.kuchukbaeva/task-1/cmd/service/main.go
+++ alyona.kuchukbaeva/task-1/cmd/service/main.go
@@ -3,38 +3,38 @@ package main
 import "fmt"
 
 func main() {
-    var a, b int
-    var op string
+	var a, b int
+	var op string
 
-    if _, err := fmt.Scanln(&a); err != nil {
-        fmt.Println("Invalid first operand")
-        return
-    }
+	if _, err := fmt.Scanln(&a); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
 
-    if _, err := fmt.Scanln(&b); err != nil {
-        fmt.Println("Invalid second operand")
-        return
-    }
+	if _, err := fmt.Scanln(&b); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
 
-    if _, err := fmt.Scanln(&op); err != nil {
-        fmt.Println("Invalid operation")
-        return
-    }
+	if _, err := fmt.Scanln(&op); err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 
-    switch op {
-    case "+":
-        fmt.Println(a + b)
-    case "-":
-        fmt.Println(a - b)
-    case "*":
-        fmt.Println(a * b)
-    case "/":
-        if b == 0 {
-            fmt.Println("Division by zero")
-            return
-        }
-        fmt.Println(a / b)
-    default:
-        fmt.Println("Invalid operation")
-    }
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
