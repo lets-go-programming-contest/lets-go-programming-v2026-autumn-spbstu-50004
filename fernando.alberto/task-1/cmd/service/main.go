b8cf4f64e537b6f0073936b9c80298522716263b
Fernando Alberto <fernandodiasfd780518@gmail.com>
2026-09-26 00:03:09 +0300

    [TASK-1] Add readInt helper

---
+++ fernando.alberto/task-1/cmd/service/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func readInt(scanner *bufio.Scanner, errorMsg string) (int, bool) {
+	scanner.Scan()
+	valueStr := strings.TrimSpace(scanner.Text())
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		fmt.Println(errorMsg)
+		return 0, false
+	}
+	return value, true
+}
+
+func main() {
+}
